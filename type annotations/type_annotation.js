94646007a028715e82aa304beb5d271437973c1e
Lamia Fatiha <f6lamia@gmail.com>
2024-07-30 12:33:19 +0600

    sring type done

--- type annotations/type_annotation.js
+++ type annotations/type_annotation.js
@@ -1,10 +1,15 @@
 var myNum = 10;
 //myNum = 'fatiha' --> type checking
 console.log(myNum.toString());
+//number type
 var summation = myNum + 12;
 console.log(summation);
 var nanValue = NaN; //valid bcz Nan is a valid number in js,ts
+//string type
 var myFName = "lamia";
 var myLName = "Fatiha";
 var add = myFName + myLName;
 console.log(add);
+var sentence = "hello! this is my typescript sentence.";
+var sentenceLenght = sentence.length;
+console.log(sentenceLenght);
--- type annotations/type_annotation.ts
+++ type annotations/type_annotation.ts
@@ -3,15 +3,20 @@ let myNum:number = 10;
 //myNum = 'fatiha' --> type checking
 
 console.log(myNum.toString());
-
+//number type
 let summation = myNum + 12;
 console.log(summation)
 
 let nanValue:number = NaN; //valid bcz Nan is a valid number in js,ts
 
+//string type
 let myFName:string = "lamia"; 
 let myLName:string = "Fatiha"; 
 
 let add = myFName + myLName;
 console.log(add);
+
+let sentence:string = "hello! this is my typescript sentence.";
+let sentenceLenght:number = sentence.length;
+console.log(sentenceLenght);
 
